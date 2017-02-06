da7d7f46e0749692a735c52781de2caad11a9ca7
Udayin <usa21@cam.ac.uk>
2017-02-06 16:35:29 +0000

    Update geo.py
    Updated Geo file

--- geo.py
+++ geo.py
@@ -2,6 +2,53 @@
 geographical data."""
 
 
+from math import radians, cos, sin, asin, sqrt
+from .utils import sorted_by_key
+
+AVG_EARTH_RADIUS = 6371  # in km
+
+def haversine(point1, point2, miles=False):
+    """ Calculate the great-circle distance between two points on the Earth surface.
+    :input: two 2-tuples, containing the latitude and longitude of each point
+    in decimal degrees.
+    Example: haversine((45.7597, 4.8422), (48.8567, 2.3508))
+    :output: Returns the distance bewteen the two points.
+    The default unit is kilometers. Miles can be returned
+    if the ``miles`` parameter is set to True.
+    """
+    # unpack latitude/longitude
+    lat1, lng1 = point1
+    lat2, lng2 = point2
+
+    # convert all latitudes/longitudes from decimal degrees to radians
+    lat1, lng1, lat2, lng2 = map(radians, (lat1, lng1, lat2, lng2))
+
+    # calculate haversine
+    lat = lat2 - lat1
+    lng = lng2 - lng1
+    d = sin(lat * 0.5) ** 2 + cos(lat1) * cos(lat2) * sin(lng * 0.5) ** 2
+    h = 2 * AVG_EARTH_RADIUS * asin(sqrt(d))
+    if miles:
+        return h * 0.621371  # in miles
+    else:
+        return h  # in kilometers
+
+def stations_by_distance(stations, p):
+    stations_list = []
+    for s in stations:
+        stations_list.append((s.name, s.town, haversine((s.coord), (p)))) 
+
+    sorted_stations_list = sorted_by_key(stations_list, 2)
+      
+    return sorted_stations_list
+    
+def stations_within_radius(stations, centre, r):
+    stations_list= []
+    a= stations_by_distance(stations, centre)
+    for s in a:
+        if s[2]<r:
+            stations_list.append(s[0])
+    return sorted(stations_list)            #Returns the list sorted
 
 """task 1d"""
 def rivers_with_station(stations):
