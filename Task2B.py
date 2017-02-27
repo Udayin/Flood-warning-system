09dc309fb4bafe06d7fe0a38ccad19a07493afad
LouiseAumont <la403@cam.ac.uk>
2017-02-27 16:59:38 +0000

    Add files via upload
    Louise 2B 2C

--- Task2B.py
+++ Task2B.py
@@ -1,28 +1,64 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 22 11:25:49 2017
 
-@author: udayinadukia
-"""
-
+# -*- coding: utf-8 -*-
+from floodsystem.station import *
 from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.utils import sorted_by_key
 from floodsystem.flood import stations_level_over_threshold
 
 
 def run():
-    
+    # Build list of stations
     stations = build_station_list()
-    update_water_levels(stations)
-    tuplist = stations_level_over_threshold(stations,0.8)
+
+    # Update latest level data for all stations
+    update_water_levels(stations) 
     
-    for i in tuplist:
-        print(i[0].name, ":", i[1])
-    #print(stations_level_over_threshold(stations, 0.8))
-   # for i in tuplist:
-     
-        
+    print(stations_level_over_threshold(stations, 0.8))
+    
+
 
 if __name__ == "__main__":
-    print("***Task 2B***")        
-run()
+    print("*** Task 2B: CUED Part IA Flood Warning System ***")
+
+    run()
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#for station in stations:
+        #if station.typical_range == None:
+            #return None
+        #if station.latest_level == None: 
+            #return None
+        #else:
+            #print("Station name and relative water level: {}, {}".format(station.name, station.relative_water_level()))
+            #print((station.latest_level - station.typical_range[0]) / (station.typical_range[1] - station.typical_range[0]))
+            #print (station.relative_water_level())
+    
+#latest = self.latest_level
+#high = self.typical_range[1]
+#low = self.typical_range[0]
+
+# print("Station name and current level: {}, {}".format(station.name,
+                                                                #  station.latest_level))
+
+# MY PROBLEM: build_station_list() RETURNS AN EMPTY LIST
+# HAVE I DEFINED update_water_levels PROPERLY??
+
+
+     # function that returns a list of tuples, where each tuple holds 
+     # (1) a station at which the latest relative water level is over tol 
+     # and (2) the relative water level at the station. 
+     # The returned list should be sorted by the relative level in descending order.
--- Task2C.py
+++ Task2C.py
@@ -1,24 +1,24 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Feb 25 15:04:37 2017
 
-@author: udayinadukia
-"""
-
+from floodsystem.station import *
 from floodsystem.stationdata import build_station_list, update_water_levels
-from floodsystem.flood import stations_highest_rel_level
+from floodsystem.utils import sorted_by_key
+from floodsystem.flood import stations_level_over_threshold, stations_highest_rel_level
+
 
 def run():
-    
-    stations= build_station_list()
-    update_water_levels(stations)
-    dangerous_stations = stations_highest_rel_level(stations, 10)
-    
-    for i in dangerous_stations:
-        print(i.name, ":", i.relative_water_level())
+    # Build list of stations
+    stations = build_station_list()
 
-if __name__ == "_main":
-    print("***Task 2C***")
+    # Update latest level data for all stations
+    update_water_levels(stations)
     
-run()
+    print (stations_highest_rel_level(stations, 10))
+    
+    
+
+if __name__ == "__main__":
+    print("*** Task 2C: CUED Part IA Flood Warning System ***")
+
+    run()
+    
