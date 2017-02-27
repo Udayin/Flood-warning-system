780fb860633635339abb90786deb386ced6c7db8
LouiseAumont <la403@cam.ac.uk>
2017-02-27 17:00:32 +0000

    Add files via upload
    Station and flood Louise

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,29 +1,26 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 22 11:40:44 2017
 
-@author: udayinadukia
-"""
-from .utils import sorted_by_key
+from floodsystem.station import *
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.utils import sorted_by_key
 
-def stations_level_over_threshold(stations, tol):
-    k= []
-    for i in stations:
-        if i.relative_water_level() == None:
+def stations_level_over_threshold(stations, tol=0):
+    """ Function that takes a list of stations and a tolerance 
+    and returns the list of tuples (name of station with relative water level over tolerance, relative water level)"""
+    S = []
+    update_water_levels(stations)
+    for station in stations:
+        if station.relative_water_level() == None:
             pass
-        elif i.relative_water_level() > tol:
-            k.append((i, i.relative_water_level()))
-    x = sorted_by_key(k,1,reverse= True )
+        if type(station.relative_water_level()) == float:
+            if station.relative_water_level() > tol:
+                S += [(station.name, station.relative_water_level())]
+    return sorted_by_key (S, 1, reverse = True)
+
     
-    return x
+    
     
     
 def stations_highest_rel_level(stations, N):
-    
-    l= []
-    b=stations_level_over_threshold(stations, 0.2)
-    for i in b:
-        l.append(i[0])
-    l_cut = l[:N]
-    return l_cut
+    S = stations_level_over_threshold(stations, tol=0)
+    return S[:N-1]
